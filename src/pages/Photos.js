daa710e451d3230a44c57d4708b8c8b0c4f06994
Miles Thomas <milesman2013@gmail.com>
2024-01-11 15:57:41 +1100

    pics imported

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import PicOne from './images/pic-one.jpg'
+import PicTwo from './images/pic-two.jpg'
+import PicThree from './images/pic-three.jpg'
+import PicFour from './images/pic-four.jpg'
+import PicFive from './images/pic-five.jpg'
+import PicSix from './images/pic-six.jpg'
+import PicSeven from './images/pic-seven.jpg'
+
 const Photos = () => {
     return (
         <div className='page'>
             <h2 className='page-header'>Photos</h2>
+            <div>
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+                <img />
+            </div>
         </div>
     )
 }
