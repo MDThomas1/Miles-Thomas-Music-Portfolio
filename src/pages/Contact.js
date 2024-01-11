613fd135de164b8159806f07e8c0ce5c0ea1ee9d
Miles Thomas <milesman2013@gmail.com>
2024-01-11 13:05:17 +1100

    contact setup

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className='page'>
+            <h2 className='page-header'>Contact</h2>
+        </div>
+    )
+}
+
+export default Contact
