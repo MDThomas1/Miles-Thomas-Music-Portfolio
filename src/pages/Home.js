5b4dd07febf1b816e86bfff11116a7d7f2b79e76
Miles Thomas <milesman2013@gmail.com>
2024-01-11 13:00:02 +1100

    home setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='home page'>
+            <h2 className='page-header'>Welcome to my portfolio!</h2>
+            <img className='profile-image' />
+        </div>
+    )
+}
+
+export default Home
