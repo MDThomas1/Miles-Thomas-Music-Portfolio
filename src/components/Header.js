66541b61edcb4c4afd4fd1d45ee58b2109d05596
Miles Thomas <milesman2013@gmail.com>
2024-01-11 12:50:18 +1100

    header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Miles Thomas' Music Portfolio</h1>
+        </div>
+    )
+}
+
+export default Header
