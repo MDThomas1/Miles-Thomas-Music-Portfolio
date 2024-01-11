b5b2175f89a7cef9b294129ab670050dec7f7026
Miles Thomas <milesman2013@gmail.com>
2024-01-11 13:08:36 +1100

    resume setup

---
+++ src/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div className='page'>
+            <h2 className='page-header'>Resume</h2>
+        </div>
+    )
+}
+
+export default Resume
