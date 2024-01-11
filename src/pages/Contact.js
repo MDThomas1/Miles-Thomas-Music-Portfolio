0e12d229886984e9f1caa723bcdd145e43ce5e87
Miles Thomas <milesman2013@gmail.com>
2024-01-11 15:11:55 +1100

    resume link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,7 @@ const Contact = () => {
     return (
         <div className='page'>
             <h2 className='page-header'>Contact</h2>
+            <p>A downloadable copy of my resume is available <a href='https://docs.google.com/document/d/1OyjX1n9Ule6O3XSEuL11g8om2LcFGqo4cUstcSyCyEY/edit#heading=h.5rf9wr4r3no2'>here</a></p>
         </div>
     )
 }
