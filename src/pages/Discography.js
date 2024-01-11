b04400f2524c27774701a43fa4f89a1f15768e93
Miles Thomas <milesman2013@gmail.com>
2024-01-11 15:21:49 +1100

    discography added

--- src/pages/Discography.js
+++ src/pages/Discography.js
@@ -4,6 +4,36 @@ const Discography = () => {
     return (
         <div className='page'>
             <h2 className='page-header'>Discography</h2>
+            <div>
+                <h3>With Dom Brinkley</h3>
+                <ul>
+                    <li><a>The Butterfly EP (Songs 2-4)</a></li>
+                    <li><a>Butterfly</a></li>
+                    <li><a>Bad Wine</a></li>
+                    <li><a>Live and Well</a></li>
+                    <li><a>Live at the Workers Club</a></li>
+                    <li><a>Sleep Talkin (Live at the Workers Club)</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>With LIV</h3>
+                <ul>
+                    <li><a>Misc</a></li>
+                    <li><a>Sunshine</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>With Swimcart</h3>
+                <ul>
+                    <li><a>The Island</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>With Rose Cohr</h3>
+                <ul>
+                    <li><a>Angela</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
