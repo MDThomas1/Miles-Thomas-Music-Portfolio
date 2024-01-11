899a91438905fde96e26e0d6125915e661f43e10
Miles Thomas <milesman2013@gmail.com>
2024-01-11 13:03:36 +1100

    bio setup

---
+++ src/pages/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile from '../images/profile.jpg'
+
+const Home = () => {
+    return (
+        <div className='page'>
+            <h2 className='page-header'>Bio</h2>
+        </div>
+    )
+}
+
+export default Home
