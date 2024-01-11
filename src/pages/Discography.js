e81f6daef383714ecacfd98fb1a78013e7814bad
Miles Thomas <milesman2013@gmail.com>
2024-01-11 15:26:10 +1100

    spotify links

--- src/pages/Discography.js
+++ src/pages/Discography.js
@@ -7,31 +7,31 @@ const Discography = () => {
             <div>
                 <h3>With Dom Brinkley</h3>
                 <ul>
-                    <li><a>The Butterfly EP (Songs 2-4)</a></li>
-                    <li><a>Butterfly</a></li>
-                    <li><a>Bad Wine</a></li>
-                    <li><a>Live and Well</a></li>
-                    <li><a>Live at the Workers Club</a></li>
-                    <li><a>Sleep Talkin (Live at the Workers Club)</a></li>
+                    <li><a href='https://open.spotify.com/album/16XIc1P170hpppPisJsY6W'>The Butterfly EP (Songs 2-4)</a></li>
+                    <li><a href='https://open.spotify.com/album/0hpb0G3Kllr3JoKxI6TZxK'>Butterfly</a></li>
+                    <li><a href='https://open.spotify.com/album/3PY21GGADKQKEyzzU9ZCqq'>Bad Wine</a></li>
+                    <li><a href='https://open.spotify.com/album/3UVUBX3pItosxipT4oQn5F'>Live and Well</a></li>
+                    <li><a href='https://open.spotify.com/album/7qSr15d1ndhywdS7CtNrgV'>Live at the Workers Club</a></li>
+                    <li><a href='https://open.spotify.com/album/225jbj2rkJM5ymr1j7vqU6'>Sleep Talkin (Live at the Workers Club)</a></li>
                 </ul>
             </div>
             <div>
                 <h3>With LIV</h3>
                 <ul>
-                    <li><a>Misc</a></li>
-                    <li><a>Sunshine</a></li>
+                    <li><a href='https://open.spotify.com/album/24r8buu8ZI0EJMeVBDvqZc'>Misc.</a></li>
+                    <li><a href='https://open.spotify.com/album/4DKSw96NB0gQkQAoMCqxp9'>Sunshine</a></li>
                 </ul>
             </div>
             <div>
                 <h3>With Swimcart</h3>
                 <ul>
-                    <li><a>The Island</a></li>
+                    <li><a href='https://open.spotify.com/album/6qCz2u4TuiT3TGzoNmF979'>The Island</a></li>
                 </ul>
             </div>
             <div>
                 <h3>With Rose Cohr</h3>
                 <ul>
-                    <li><a>Angela</a></li>
+                    <li><a href='https://open.spotify.com/album/4HYzxHrCsW8GOE8iN3Vihl'>Angela</a></li>
                 </ul>
             </div>
         </div>
