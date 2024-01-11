f3712f27380b49db7bc8ef31a1cb8456ce0f3909
Miles Thomas <milesman2013@gmail.com>
2024-01-11 15:52:49 +1100

    home pic added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import HomePic from './images/home-pic.jpg'
+
 const Home = () => {
     return (
         <div className='home page'>
             <h2 className='page-header'>Welcome to my portfolio!</h2>
-            <img className='profile-image' />
+            <img className='profile-image' src={HomePic} />
         </div>
     )
 }
