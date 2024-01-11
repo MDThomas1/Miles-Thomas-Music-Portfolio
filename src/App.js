7150bdfaf447a637db714698486cfe3f72595117
Miles Thomas <milesman2013@gmail.com>
2024-01-11 13:16:34 +1100

    app page imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+import Home from './pages/Home'
+import Bio from './pages/Bio'
+import Discography from './pages/Discography'
+import Photos from './pages/Photos'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
 
 function App() {
   return (
